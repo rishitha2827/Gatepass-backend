21e9881abd6f37c4a45f64b429f28bbe64c15589
Rishitha1238 <141536317+rishitha2827@users.noreply.github.com>
2025-05-27 22:50:22 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,11 +6,4 @@ router.post('/login', login);
 
 const supabase = require('../supabaseClient');
 
-router.get('/test-users', async (req, res) => {
-  const { data, error } = await supabase.from('users').select('*');
-  if (error) return res.status(500).json({ error: error.message });
-  res.json(data);
-});
-
-
 module.exports = router;
